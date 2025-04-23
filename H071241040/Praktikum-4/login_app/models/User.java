e8f0ddf83ad4575c450918b698d6764bb4a132b8
RfqiAlan <rifqialan46475@gmail.com>
2025-04-23 13:47:36 +0700

    PUSH TP 1-4

---
+++ H071241040/Praktikum-4/login_app/models/User.java
@@ -0,0 +1,29 @@
+// File: User.java
+package login_app.models;
+
+public class User {
+     /*
+     * TODO:
+     * Tambahkan attribute, method, atau constructor
+     * yang dibutuhkan di kelas user
+     */
+    private String username;
+    private String password;
+
+  
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    
+
+}
