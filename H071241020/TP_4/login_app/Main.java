eec038f51b631f5b402ea6db7ef4ec8b1661f144
Agung4513 <alloagung@gmail.com>
2025-05-06 14:18:07 +0800

    Praktikum-4

---
+++ H071241020/TP_4/login_app/Main.java
@@ -0,0 +1,120 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import models.Profile;
+import models.User;
+import utils.StringUtils;
+
+public class Main {
+    private static ArrayList<User> listUser = new ArrayList<>();
+    private static ArrayList<Profile> listUserProfile = new ArrayList<>();
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        runApp();
+    }
+
+    private static void runApp() {
+        System.out.println("-------------------------");
+        System.out.println("Aplikasi Login Sederhana");
+        System.out.println("-------------------------");
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        System.out.print("> ");
+
+        int selectMenu = sc.nextInt();
+        sc.nextLine();
+        switch (selectMenu) {
+            case 1:
+                showLoginMenu();
+                break;
+            case 2:
+                showRegisterMenu();
+                break;
+            default:
+                runApp();
+        }
+    }
+
+    private static void showLoginMenu() {
+        System.out.println("-------------------------");
+        System.out.println("Login");
+        System.out.println("Masukkan Username");
+        System.out.print("> ");
+
+        String username = sc.next();
+        int userIndex = -1;
+        for (int i = 0; i < listUser.size(); i++) {
+            if (listUser.get(i).getUsername().equals(username)) {
+                userIndex = i;
+                break;
+            }
+        }
+
+        if (userIndex != -1) {
+            System.out.println("Password");
+            System.out.print("> ");
+            String password = sc.next();
+            boolean isPasswordMatch = listUser.get(userIndex).getPassword().equals(password);
+
+            if (isPasswordMatch) {
+                System.out.println("Berhasil Login");
+                showDetailUser(listUserProfile.get(userIndex));
+                System.exit(0);
+            } else {
+                System.out.println("Password Salah");
+                runApp(); // kembali ke menu utama
+            }
+        } else {
+            System.out.println("Username Tidak Ditemukan");
+            runApp(); // kembali ke menu utama
+        }
+    }
+
+    private static void showRegisterMenu() {
+        System.out.println("-------------------------");
+        System.out.println("REGISTER");
+
+        System.out.println("Username");
+        System.out.print("> ");
+        String username = sc.nextLine();
+        System.out.println("Password");
+        System.out.print("> ");
+        String password = sc.nextLine();
+        
+        User user = new User(username, password);
+        Profile profile = new Profile();
+
+        System.out.println("Nama Lengkap");
+        System.out.print("> ");
+        String fullName = sc.nextLine();
+        System.out.println("Umur");
+        System.out.print("> ");
+        int age = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Hobby");
+        System.out.print("> ");
+        String hobby = sc.nextLine();
+
+        profile.setFullName(fullName);
+        profile.setAge(age);
+        profile.setHobby(hobby);
+        profile.setNickName(StringUtils.getNickName(fullName));
+
+        listUser.add(user);
+        listUserProfile.add(profile);
+
+        System.out.println("-------------------------");
+        System.out.println("Berhasil Membuat User Baru!!");
+        runApp();
+    }
+
+    private static void showDetailUser(Profile profile) {
+        System.out.println("-------------------------");
+        System.out.println("Detail User");
+        System.out.println("Nama Lengkap : " + profile.getFullName());
+        System.out.println("Nickname     : " + profile.getNickName());
+        System.out.println("Umur         : " + profile.getAge());
+        System.out.println("Hobby        : " + profile.getHobby());
+        System.out.println("-------------------------");
+    }
+}
---
+++ H071241020/TP_4/login_app/models/Profile.java
@@ -0,0 +1,40 @@
+package models;
+
+public class Profile {
+    private String fullName;
+    private String nickName;
+    private int age;
+    private String hobby;
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+}
